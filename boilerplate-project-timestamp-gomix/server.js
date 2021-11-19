0cab4acbfaec82af4c98dcc97bd58d9eb04e6018
thomsult <thomsult@gmail.com>
2021-11-19 17:09:57 +0000

    All is ok but it's not optimal

--- boilerplate-project-timestamp-gomix/server.js
+++ boilerplate-project-timestamp-gomix/server.js
@@ -5,27 +5,26 @@
 var express = require('express');
 var app = express();
 var crypto = require('crypto');
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 const { query } = require('express');
 var shorturlArray = []
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: false}))
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
 
+app.use(express.static(__dirname +'/views/'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-var Log = [];
-app.get("/users" , function (req, res) {res.sendFile(__dirname + '/views/users.html')});
+var LogUser = [];
+app.get("/users" , function (req, res) {res.status(200).sendFile(__dirname + '/views/users.html')});
 app.get("/api/users", function (req, res) {
   var Users = [];
-  Log.forEach(function (userid) {
+  LogUser.forEach(function (userid) {
     var user = {
       username:userid['username'],
       _id:userid['_id']
@@ -33,10 +32,10 @@ app.get("/api/users", function (req, res) {
     Users.push(user)
   });
   
-  res.json(Users);})
+  res.status(200).json(Users);})
 
 app.post('/api/users',(req, res)=>{
-  console.log(req.body.username)
+  //console.log(req.body.username)
   if(req.body.username != ''){
     var id = crypto.createHash('md5').update(req.body.username).digest('hex');
   
@@ -47,64 +46,59 @@ app.post('/api/users',(req, res)=>{
       log:[]
     }
   
-    Log.push(userID);
-    res.json({
+    LogUser.push(userID);
+    res.status(200).json({
       username:req.body.username,
       _id:id
     })
   }
   else{
-    res.json({ error: 'invalid username' })
+    res.status(200).json({ error: 'invalid username' })
   }
   
 })
 app.post('/api/users/:_id/exercises',(req, res)=>{
-  //console.log(req.body[':_id']);
-  Log.forEach(function (userid) {
-    if(userid['_id'] == req.body[':_id']){
 
+  //console.log(req.params._id+" " + JSON.stringify(req.body));
+for (i = 0; i < LogUser.length; ++i) {
+    if(LogUser[i]._id == req.params._id){
+      var id = i;
+      LogUser[i].count++;
+      LogUser[i].log.push({description: req.body['description'],
+        duration: chekDuration(req.body['duration']),
+        date: chekDate(req.body['date'])})
+        break
+    }
 
-      userid.count++;
-
-
-      userid.log.push({
+  }
+  res.status(200).json({
+        username: LogUser[id].username,
         description: req.body['description'],
         duration: chekDuration(req.body['duration']),
         date: chekDate(req.body['date']),
+        _id: LogUser[id]._id
       })
+    
+    
 
-      res.json({
-        username: userid.username,
-        description: req.body['description'],
-        duration: chekDuration(req.body['duration']),
-        date: chekDate(req.body['date']),
-        _id: userid._id
-      })
-      
-    }
-    else{
-      res.json({ error: 'invalid _id' })
+    
 
-    }
-
-  })})
+  });
 
   function chekDate(strg){
 
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     var d = new Date(strg);
     if (d == "Invalid Date") {
-      var sd = new Date();
-        return sd.toLocaleDateString(undefined,options)
+        return new Date().toDateString() 
     }
     else{
-      return d.toLocaleDateString(undefined,options)
+      return new Date(strg).toDateString() 
     }
   }
 
 function chekDuration(Num){
     if(Num > 1){
-      return Num
+      return parseInt(Num)
 
     }
     else{
@@ -133,51 +127,76 @@ function chekDuration(Num){
 
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-    var start = new Date(req.query.from);
-    var to = new Date(req.query.to);
-    var limit = parseInt(req.query.limit);
+    var start = new Date(req.query.from) != "Invalid Date"?new Date(req.query.from):"";
+    var to = new Date(req.query.to)!= "Invalid Date"?new Date(req.query.to):"";
+    var limit = 0;
+    req.query.limit != undefined? limit = parseInt(req.query.limit) :limit = null;
 
     console.log(start,to,limit)
-    Log.forEach(function (userid) {
-      if(userid._id == req.params._id){
-        if(userid.log.length > limit){
-          var logFilted =  []
-          const promise1 = new Promise((resolve, reject) => {
-            for (i = 0; i < userid.log.length; ++i) {
-              if(logFilted.length < limit){
-               if(userid.log[i].date >= start.toLocaleDateString(undefined,options) && userid.log[i].date <= to.toLocaleDateString(undefined,options)){
-                 logFilted.push(userid.log[i])
-               }
+    logReply = []
+    id = 0
+
+
+//new  &&
+    for (i = 0; i < LogUser.length; ++i){
+      if(LogUser[i]._id == req.params._id){
+        id = i;
+        if(start == "" && to == "" && limit == null){
+      
+          logReply = [...LogUser[i].log]
+
+        }
+        else if (start != "" && to != "" || limit != null){
+          for (j = 0; j < LogUser[i].log.length; ++j){
+            
+            if( logReply.length < limit && limit != null){
+              if(start != "" && to != ""){
+                console.log(new Date(LogUser[i].log[j].date) +" " + start);
+                if(new Date(LogUser[i].log[j].date) > start && new Date(LogUser[i].log[j].date) < to){
+                  logReply.push(LogUser[i].log[j])
+                  console.log(LogUser[i].log[j] + "start");
+                }
+              
+              }else{
+                logReply.push(LogUser[i].log[j])
+                console.log(LogUser[i].log[j] + "nostart");
               }
-              resolve()
-           }
-          });
-         
-          promise1.then(() => {
-            res.json({
-              username: userid.username,
-              _id: userid._id,
-              count:userid.count,
-              log:logFilted
-            })
-          });
-         
-        
+              
+              
+            }else if (limit == null){
+              if(start != "" && to != ""){
+                console.log(new Date(LogUser[i].log[j].date) +" " + start);
+                if(new Date(LogUser[i].log[j].date) > start && new Date(LogUser[i].log[j].date) < to){
+                  logReply.push(LogUser[i].log[j])
+                  console.log(LogUser[i].log[j] + "start");
+                }
+              
+              }else{
+                logReply.push(LogUser[i].log[j])
+                console.log(LogUser[i].log[j] + "nostart");
+              }
+            }
+            
+            
+            else{
+
+              break
+            }
+            
+          }
+          
         }
-        else{
-          res.json(userid);
-        }
-        
-
-      }
-
-
-    })
+    }
+   
     
     
-    
-  });
-
+  };
+ res.status(200).json({
+              username: LogUser[id].username,
+              _id: LogUser[id]._id,
+              count:LogUser[id].count,
+              log:logReply
+            })})
 
 
 
@@ -189,7 +208,7 @@ app.get("/api/hello", function (req, res) {
 });
 //api short url
 app.post('/api/shorturl',(req, res) =>{
-  console.log(req.body.url)
+  //console.log(req.body.url)
   var url = req.body.url
   var regex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/
   //regex match if url is similar to  http://www.example.com 
@@ -208,26 +227,26 @@ app.post('/api/shorturl',(req, res) =>{
   
 app.get('/api/shorturl/:id',(req, res) =>{
 
-  console.log(req.params.id)
+  //console.log(req.params.id)
   var URLbyid = shorturlArray[req.params.id];
   res.redirect(URLbyid)
 })
 
 //api date + whoami
 app.get('/api/date/:date',(req, res) => {
-    console.log(req.params.date)
+    //console.log(req.params.date)
 
     if(req.params.date ){
       if(req.params.date.match(/^(\d){1,}$/)){
         var dates = new Date(parseInt(req.params.date))
-        console.log(dates)
+        //console.log(dates)
         res.json({"unix":parseInt(req.params.date),"utc":dates.toUTCString()})
       }
       else{
         if(new Date(req.params.date) != "Invalid Date" ){
 
           var dates = new Date(req.params.date)
-          console.log(dates)
+          //console.log(dates)
           res.json({"unix":dates.getTime(),"utc":dates.toUTCString()})
         }
         else if(req.params.date == 'whoami'){
@@ -262,7 +281,7 @@ app.get('/api/date/:date',(req, res) => {
 })
 
 app.get('/api/date',(req, res) => { var dates = new Date()
-          console.log(dates+"get/")
+          //console.log(dates+"get/")
           res.json({"unix":dates.getTime(),"utc":dates.toUTCString()})
 })
 
